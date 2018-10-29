da0d0021ee70c13efff004631b98998e599decc7
melonholy <36301981+melonholy@users.noreply.github.com>
2018-10-30 01:18:28 +0300

    Fix sort

--- src/services/services.js
+++ src/services/services.js
@@ -1,31 +1,44 @@
-const fs=require('fs')
-const films =JSON.parse(fs.readFileSync('data.json','utf8'))
+const fs = require("fs");
+const films = JSON.parse(fs.readFileSync("data.json", "utf8"));
 
 function getServices() {
-    const searchService = (film)=> {
-        return films.filter(item => item.title.toString().toLowerCase().includes(film)).map(item => item.title)
-    }
+  const searchService = film => {
+    return films
+      .filter(item =>
+        item.title
+          .toString()
+          .toLowerCase()
+          .includes(film)
+      )
+      .map(item => item.title);
+  };
 
-    const pageService = (offset,limit)=> {
-        return films.slice(Number(offset),Number(offset) + Number(limit)).map(item => item.title)
-    }
+  const pageService = (offset, limit) => {
+    return films
+      .slice(Number(offset), Number(offset) + Number(limit))
+      .map(item => item.title);
+  };
 
-    const sortService = (FieldOfFilm,direction) =>{
-     return (films.sort((a,b)=>{
-            if(direction === 'l')
-                return a[FieldOfFilm].localeCompare(b[FieldOfFilm]);
-            else if (direction === 'r')
-                return b[FieldOfFilm].localeCompare(a[FieldOfFilm])
-        }).map(item=>item.title))
-    }
+  const sortService = (FieldOfFilm, direction) => {
+    return films
+      .sort((a, b) => {
+        if (direction === "l")
+          return String(a[FieldOfFilm]).localeCompare(String(b[FieldOfFilm]));
+        else if (direction === "r")
+          return String(b[FieldOfFilm]).localeCompare(String(a[FieldOfFilm]));
+      })
+      .map(item => item.title);
+  };
 
-    const idService = (ID) =>{
-        return (films.filter(item=>{
-            return item.id===Number(ID)
-        }).map(item=>item.title))
-    }
+  const idService = ID => {
+    return films
+      .filter(item => {
+        return item.id === Number(ID);
+      })
+      .map(item => item.title);
+  };
 
-    return {searchService,pageService,sortService,idService}
+  return { searchService, pageService, sortService, idService };
 }
 
 module.exports = getServices();
