0d56a5895ce1db0e8edcfc4bac1d433566308130
melonholy <36301981+melonholy@users.noreply.github.com>
2018-12-18 02:12:53 +0300

    without AdQu

---
+++ src/services/services.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/local");
+const schema = mongoose.Schema({
+  vote_count: {
+    type: Number
+  },
+  id: {
+    type: Number
+  },
+  video: {
+    type: Boolean
+  },
+  vote_average: {
+    type: Number
+  },
+  title: {
+    type: String
+  },
+  popularity: {
+    type: Number
+  },
+  poster_path: {
+    type: String
+  },
+  original_language: {
+    type: String
+  },
+  original_title: {
+    type: String
+  },
+  genre_ids: {
+    type: Array
+  },
+  backdrop_path: {
+    type: String
+  },
+  adult: {
+    type: Boolean
+  },
+  overview: {
+    type: String
+  },
+  release_date: {
+    type: String
+  }
+});
+const Films = mongoose.model("Films", schema);
+
+function getServices() {
+  const searchService = film => {
+    Films.find({})
+      .where("title", new RegExp(film, "i"))
+      .then(films => res.json(films));
+  };
+
+  const showService = res => {
+    Films.find({}).then(films => {
+      res.json(films);
+    });
+  };
+
+  const pageService = (offset, limit) => {
+    Films.find()
+      .limit(limit)
+      .skip(offset)
+      .then(films => res.json(films));
+  };
+
+  const sortService = (FieldOfFilm, direction) => {
+    if (direction === "l")
+      Films.find({})
+        .sort([[title, -1]])
+        .then(films => res.json(films));
+    else if (direction === "r")
+      Films.find({})
+        .sort([[title, 1]])
+        .then(films => res.json(films));
+  };
+
+  const idService = (ID, res) => {
+    Films.findOne({ ID }).then(film => {
+      res.json(film);
+    });
+  };
+
+  return { searchService, pageService, sortService, idService, showService };
+}
+
+module.exports = getServices();
